050f6bb9ac9394cf1df331d3be4f5d2415dc8ac5
Omar Sarabia <elsarabia@hotmail.com>
2019-10-25 21:12:03 -0700

    point BinaryTree

--- findduplicatespaceedition/main.go
+++ findduplicatespaceedition/main.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+/*
+Find a duplicate, Space Edition™.
 
+We have a list of integers, where:
+
+The integers are in the range 1..n1..n
+The list has a length of n+1n+1
+It follows that our list has at least one integer which appears at least twice. But it may have several duplicates, and each duplicate may appear more than twice.
+
+Write a function which finds an integer that appears more than once in our list. (If there are multiple duplicates, you only need to find one of them.)
+
+We're going to run this function on our new, super-hip MacBook Pro With Retina Display™. Thing is, the damn thing came with the RAM soldered right to the motherboard, so we can't upgrade our RAM. So we need to optimize for space!
+*/
 
 func main() {
 	input1 := []int{1, 2, 3, 2}
